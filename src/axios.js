3a4ebcc2c73a1aa9f9c106009d57b22162d8431d
AryakRoy <aryakroy63@gmail.com>
2021-12-06 10:10:23 +0530

    Deployment Setup Complete

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const instance = axios.create({
-    baseURL: 'http://localhost:4999/api',
+    baseURL: 'https://mysterious-temple-25929.herokuapp.com/api',
     headers: {
         "content-type": "application/json"
     }
